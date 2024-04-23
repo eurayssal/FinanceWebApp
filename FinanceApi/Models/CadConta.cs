2c5b5a2f91bcf6697ba6c90bcff6204d08352034
eurayssal <eurayssamagalhaes@gmail.com>
2024-04-23 16:28:49 -0300

    Model cadConta

---
+++ FinanceApi/Models/CadConta.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace FinanceApi.Models
+{
+    public class CadConta
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string Nome { get; set; }
+
+        public double Saldo { get; set; }
+        public bool Atividade { get; set; }
+    }
+}
