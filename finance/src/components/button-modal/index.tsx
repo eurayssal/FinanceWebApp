e2d80a0e6f35dab7ac79f9069ca767bd6e5a68fd
eurayssal <eurayssamagalhaes@gmail.com>
2024-09-23 14:23:56 -0300

    Ajustes button modal

--- finance/src/components/button-modal/index.tsx
+++ finance/src/components/button-modal/index.tsx
@@ -4,7 +4,7 @@ import ButtonUi from '../button';
 import ModalUi from '../modal';
 
 const ButtonModalUi: React.FC<IButtonModalUiProps> = (props) => {
-    const { modal, onReload, ...buttonUiProps } = props;
+    const { modal, onReload, titleModal, ...buttonUiProps } = props;
 
     const [open, setOpen] = React.useState(false);
 
@@ -20,7 +20,7 @@ const ButtonModalUi: React.FC<IButtonModalUiProps> = (props) => {
 
     return (<>
         <ButtonUi {...buttonUiProps} onClick={handleOpen} />
-        <ModalUi content={modal} open={open} {...modalProps} />
+        <ModalUi title={titleModal} content={modal} open={open} {...modalProps} />
     </>)
 }
 
