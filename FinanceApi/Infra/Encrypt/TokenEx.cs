efb14b35161e78f5caaead790c05bc2fc92149b1
eurayssal <eurayssamagalhaes@gmail.com>
2024-10-01 17:12:06 -0300

    Token de login com 1 ano

--- FinanceApi/Infra/Encrypt/TokenEx.cs
+++ FinanceApi/Infra/Encrypt/TokenEx.cs
@@ -19,7 +19,8 @@ namespace FinanceApi.Infra.Encrypt
                 [
                     new Claim(type: ClaimTypes.NameIdentifier, value: model.Id.ToString()),
                 ]),
-                Expires = DateTime.UtcNow.AddMinutes(value: jwtSettings.AccessTokenExpirationMinutes),
+                //Expires = DateTime.UtcNow.AddMinutes(value: jwtSettings.AccessTokenExpirationMinutes),
+                Expires = DateTime.UtcNow.AddYears(1),
                 SigningCredentials = new SigningCredentials(key: new SymmetricSecurityKey(key), algorithm: SecurityAlgorithms.HmacSha256Signature)
             };
 
