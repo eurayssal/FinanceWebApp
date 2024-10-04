97c710ca020a83c79c9c2d6ea3fc902a12e9b180
eurayssal <eurayssamagalhaes@gmail.com>
2024-10-04 17:07:43 -0300

    Padronização de codigo

--- finance/src/contexts/props.ts
+++ finance/src/contexts/props.ts
@@ -8,87 +8,97 @@ export interface ICurrentColorPallete {
     colorBorder: IColorBorder,
     fill: IFill,
     background: IBackground,
+    shadow: IShadow
 }
+
 interface IColorPrimary {
-    colorPrimaryBg: string,
-    colorPrimaryBgHover: string,
-    colorPrimaryBorder: string,
-    colorPrimaryBorderHover: string,
-    colorPrimaryHover: string,
-    colorPrimary: string,
-    colorPrimaryActive: string,
-    colorPrimaryTextHover: string,
-    colorPrimaryText: string,
-    colorPrimaryTextActive: string
+    colorPrimaryBg: string;
+    colorPrimaryBgHover: string;
+    colorPrimaryBorder: string;
+    colorPrimaryBorderHover: string;
+    colorPrimaryHover: string;
+    colorPrimary: string;
+    colorPrimaryActive: string;
+    colorPrimaryTextHover: string;
+    colorPrimaryText: string;
+    colorPrimaryTextActive: string;
 }
 
 interface IColorSuccess {
-    colorSuccessBg: string,
-    colorSuccessBgHover: string,
-    colorSuccessBorder: string,
-    colorSuccessBorderHover: string,
-    colorSuccessHover: string,
-    colorSuccess: string,
-    colorSuccessActive: string,
-    colorSuccessTextHover: string,
-    colorSuccessText: string,
-    colorSuccessTextActive: string,
+    colorSuccessBg: string;
+    colorSuccessBgHover: string;
+    colorSuccessBorder: string;
+    colorSuccessBorderHover: string;
+    colorSuccessHover: string;
+    colorSuccess: string;
+    colorSuccessActive: string;
+    colorSuccessTextHover: string;
+    colorSuccessText: string;
+    colorSuccessTextActive: string;
 }
 
 interface IColorWarning {
-    colorWarningBg: string,
-    colorWarningBgHover: string,
-    colorWarningBorder: string,
-    colorWarningBorderHover: string,
-    colorWarningHover: string,
-    colorWarning: string,
-    colorWarningActive: string,
-    colorWarningTextHover: string,
-    colorWarningText: string,
-    colorWarningTextActive: string,
+    colorWarningBg: string;
+    colorWarningBgHover: string;
+    colorWarningBorder: string;
+    colorWarningBorderHover: string;
+    colorWarningHover: string;
+    colorWarning: string;
+    colorWarningActive: string;
+    colorWarningTextHover: string;
+    colorWarningText: string;
+    colorWarningTextActive: string;
 }
 
 interface IColorError {
-    colorErrorBg: string,
-    colorErrorBgHover: string,
-    colorErrorBorder: string,
-    colorErrorBorderHover: string,
-    colorErrorHover: string,
-    colorError: string,
-    colorErrorActive: string,
-    colorErrorTextHover: string,
-    colorErrorText: string,
-    colorErrorTextActive: string,
+    colorErrorBg: string;
+    colorErrorBgHover: string;
+    colorErrorBorder: string;
+    colorErrorBorderHover: string;
+    colorErrorHover: string;
+    colorError: string;
+    colorErrorActive: string;
+    colorErrorTextHover: string;
+    colorErrorText: string;
+    colorErrorTextActive: string;
 }
 
 interface IColorLink {
-    colorLinkHover: string,
-    colorLinkActive: string
+    colorLinkHover: string;
+    colorLinkActive: string;
 }
 
 interface IColorText {
-    colorText: string,
-    colorTextSecondary: string,
-    colorTextTertiary: string,
-    colorTextQuaternary: string,
+    colorTextBase: string;
+    colorText: string;
+    colorTextSecondary: string;
+    colorTextTertiary: string;
+    colorTextQuaternary: string;
 }
 
 interface IColorBorder {
-    colorBorder: string
-    colorBorderSecondary: string
+    colorBorder: string;
+    colorBorderSecondary: string;
 }
 
 interface IFill {
-    colorFill: string,
-    colorFillSecondary: string,
-    colorFillTertiary: string,
-    colorFillQuaternary: string,
+    colorFill: string;
+    colorFillSecondary: string;
+    colorFillTertiary: string;
+    colorFillQuaternary: string;
 }
 
 interface IBackground {
-    colorBgContainer: string
-    colorBgElevated: string
-    colorBgLayout: string
-    colorBgSpotlight: string
-    colorBgMask: string
+    colorBgBase: string;
+    colorBgContainer: string;
+    colorBgElevated: string;
+    colorBgLayout: string;
+    colorBgSpotlight: string;
+    colorBgMask: string;
+}
+
+interface IShadow {
+    defaultShadow: string;
+    dangerShadow: string;
+    primaryShadow: string;
 }
