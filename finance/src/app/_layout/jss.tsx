773c247e8d10192fc2b5aa0f649898cb95ae201a
eurayssal <eurayssamagalhaes@gmail.com>
2024-10-04 17:40:11 -0300

    Ajuste cores do layout

--- finance/src/app/_layout/jss.tsx
+++ finance/src/app/_layout/jss.tsx
@@ -1,4 +1,6 @@
 import styled from "@emotion/styled";
+import { useContext } from "react";
+import ThemeContext from "../../contexts";
 
 //container Pai
 export const AppLayoutJss = styled.div({
@@ -9,13 +11,31 @@ export const AppLayoutJss = styled.div({
 
 // TopBar
 
-export const TopBar = styled.div({
-    paddingLeft: '2rem',
-    paddingRight: '2rem',
-    display: "flex",
-    alignItems: 'center',
-    justifyContent: "space-around",
-    backgroundColor: '#0958D9',
+export const TopBar = styled.div(() => {
+    const theme = useContext(ThemeContext);
+    return {
+        paddingLeft: '2rem',
+        paddingRight: '2rem',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: "space-around",
+        backgroundColor: theme.pallete.background.colorBgContainer,
+        borderBottom: `1px solid ${theme.pallete.colorBorder.colorBorderSecondary}`
+    }
+});
+
+
+export const Footer = styled.div(() => {
+    const theme = useContext(ThemeContext);
+    return {
+        color: theme.pallete.colorText.colorText,
+        display: 'flex',
+        backgroundColor: theme.pallete.background.colorBgContainer,
+        alignItems: 'center',
+        padding: '12px',
+        marginTop: 'auto',
+        justifyContent: 'center'
+    }
 });
 
 export const LeftConteiner = styled.div({
@@ -31,12 +51,6 @@ export const CenterContainer = styled.div({
 export const RigthConteiner = styled.div({
 });
 
-export const Footer = styled.div({
-    backgroundColor: "#fff",
-    textAlign: "center",
-    padding: "12px",
-    marginTop: "auto",
-});
 
 export const LayoutContainerJss = styled.div({
     margin: '16px'
